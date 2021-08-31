303bb83165a923bdc6e59345db42344ce32a1dac
Beket <lbeketl@gmail.com>
2021-08-31 21:44:44 +0300

    Finish commit

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,6 +1,7 @@
+import { Publish } from '@material-ui/icons';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Chart from '../../components/chart/Chart'
+import Chart from '../../components/chart/Chart';
 import { productData } from '../../dummyData';
 import './product.css';
 
@@ -14,54 +15,72 @@ function Product() {
         </Link>
       </div>
       <div className="productTop">
-          <div className="productTopLeft">
-            <Chart data={productData} dataKey="Sales" title="Sales Performance"/>
+        <div className="productTopLeft">
+          <Chart data={productData} dataKey="Sales" title="Sales Performance" />
+        </div>
+        <div className="productTopRight">
+          <div className="productInfoTop">
+            <img
+              className="productInfoImg"
+              src="https://ilounge.ua/files/products/apple-airpods-2-sweet-lilac-mv7n2_1.1000x.jpeg"
+              alt=""
+            />
+            <span className="productName">Apple Airpods</span>
           </div>
-          <div className="productTopRight">
-            <div className="productInfoTop">
-                <img className="productInfoImg" src="https://ilounge.ua/files/products/apple-airpods-2-sweet-lilac-mv7n2_1.1000x.jpeg" alt=""  />
-                <span className="productName">Apple Airpods</span>
+          <div className="productInfoBottom">
+            <div className="productInfoItem">
+              <span className="productInfoKey">id: </span>
+              <span className="productInfoValue">123</span>
             </div>
-            <div className="productInfoBottom">
-               <div className="productInfoItem">
-               <span className="productInfoKey">id: </span>
-                <span className="productInfoValue">123</span>
-               </div>
-               <div className="productInfoItem">
-               <span className="productInfoKey">sales: </span>
-                <span className="productInfoValue">563</span>
-               </div>
-               <div className="productInfoItem">
-               <span className="productInfoKey">active: </span>
-                <span className="productInfoValue">yes</span>
-               </div>
-               <div className="productInfoItem">
-               <span className="productInfoKey">in stock: </span>
-                <span className="productInfoValue">no</span>
-               </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">sales: </span>
+              <span className="productInfoValue">563</span>
+            </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">active: </span>
+              <span className="productInfoValue">yes</span>
+            </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">in stock: </span>
+              <span className="productInfoValue">no</span>
             </div>
           </div>
+        </div>
       </div>
       <div className="productBottom">
-          <form action="
-          " className="productForm">
-              <div className="productFormLeft">
-                  <label htmlFor="">Product Name</label>
-                  <input type="text"  placeholder="Apple Airpods"/>
-                  <select name="inStock" id="idStock">
-                      <option value="yes">Yes</option>
-                      <option value="no">No</option>
-                  </select>
-                  <label htmlFor="">Active</label>
-                  <select name="inStock" id="idStock">
-                      <option value="yes">Yes</option>
-                      <option value="no">No</option>
-                  </select>
-              </div>
-              <div className="productFormRight">
-
-              </div>
-          </form>
+        <form
+          action="
+          "
+          className="productForm">
+          <div className="productFormLeft">
+            <label htmlFor="">Product Name</label>
+            <input type="text" placeholder="Apple Airpods" />
+            <label htmlFor="">In Stock</label>
+            <select name="inStock" id="idStock">
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+            <label htmlFor="">Active</label>
+            <select name="active" id="active">
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+          </div>
+          <div className="productFormRight">
+            <div className="productUpload">
+              <img
+                className="productUploadImg"
+                src="https://www.sbsmobile.com/dan/170737-thickbox_default/silicone-case-for-apple-airpods-1-2.jpg"
+                alt=""
+              />
+              <label htmlFor="file">
+                <Publish />
+              </label>
+              <input type="file" name="file" id="file" style={{ display: 'none' }} />
+            </div>
+            <button className="productButton">Update</button>
+          </div>
+        </form>
       </div>
     </div>
   );
